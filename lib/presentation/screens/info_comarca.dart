97ea018612086828fc660c4408839f461d4b1483
MarcosGit2005 <marcosrubiodiaz@gmail.com>
2025-01-23 18:37:02 +0100

    23012025 U5PA1

--- lib/presentation/screens/info_comarca.dart
+++ lib/presentation/screens/info_comarca.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import '../../data/peticions_http.dart';
 import '../../data/counties.dart';
 
 class ComarcaInfo extends StatelessWidget {
@@ -13,7 +13,7 @@ class ComarcaInfo extends StatelessWidget {
       appBar: AppBar(
         title: Text(provincies["provincies"][idProvince]["comarques"][idComarca]["comarca"]), // Título de la cabecera
       ),
-      body: ComarcaInfoScreen(idProvince: idProvince,idComarca: idComarca),
+      body: ComarcaInfoScreen(idProvince: idProvince,idComarca: idComarca,),
     );
   }
 }
@@ -31,7 +31,20 @@ class _ComarcaInfoScreenState extends State<ComarcaInfoScreen> {
   final int idProvince;
   final int idComarca;
   _ComarcaInfoScreenState({required this.idProvince, required this.idComarca});
+
+  late Future<dynamic> info;
   bool showFirstContent = true;
+  double latitud = 0;
+  double longitud = 0;
+
+  @override
+  void initState() {
+    latitud = provincies["provincies"][idProvince]["comarques"][idComarca]["coordenades"][0];
+    longitud = provincies["provincies"][idProvince]["comarques"][idComarca]["coordenades"][1];
+
+    info = obteClima(
+        longitud: longitud, latitud: latitud);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -108,77 +121,161 @@ class _ComarcaInfoScreenState extends State<ComarcaInfoScreen> {
   }
 
   Widget _buildSecondContent() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        // Imagen en la parte superior
-        Center(
-            child: Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/img/weather.png'), // Ruta de la imagen
-                  fit: BoxFit.cover,
-                ),
+    return FutureBuilder(
+      future: info,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          // Una vegada es resol el Future, tindrem disponible
+          // la informació necessària per construir el giny
+          String temperatura =
+          snapshot.data["current_weather"]["temperature"].toString();
+          String velVent =
+          snapshot.data["current_weather"]["windspeed"].toString();
+          String direccioVent =
+          snapshot.data["current_weather"]["winddirection"].toString();
+          String codi =
+          snapshot.data["current_weather"]["weathercode"].toString();
+
+          return Column(
+            children: [
+              _obtenirIconaOratge(codi),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.thermostat,
+                    size: 35,
+                  ),
+                  Text(
+                    "$temperaturaº",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
               ),
-            )
-        ),
-        SizedBox(height: 8),
-        // Temperatura
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.thermostat,
-              size: 24,
-              color: Colors.grey[600],
-            ),
-            SizedBox(width: 4),
-            Text(
-              '15.4°',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[700],
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.wind_power, size: 35),
+                  const SizedBox(width: 30),
+                  Text(
+                    "${velVent}km/h",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(width: 30),
+                  _obteGinyDireccioVent(double.parse(direccioVent)),
+                ],
               ),
-            ),
-          ],
-        ),
-        SizedBox(height: 8),
-        // Velocidad y dirección del viento
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.air,
-              size: 24,
-              color: Colors.grey[600],
-            ),
-            SizedBox(width: 4),
-            Text(
-              '9.4km/h Ponent',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[700],
+              const SizedBox(height: 20),
+              Text(
+                "Latitud: $latitud",
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
-            ),
-          ],
-        ),
-        SizedBox(height: 16),
-        // Información adicional
-        Center(
-          child: Text(
-            'Població: '+provincies["provincies"][idProvince]["comarques"][idComarca]["poblacio"]+'\nLatitud: '+provincies["provincies"][idProvince]["comarques"][idComarca]["coordenades"][0].toString()+
-                '\nLongitud: '+provincies["provincies"][idProvince]["comarques"][idComarca]["coordenades"][1].toString(),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey[700],
-            ),
-          ),
-        ),
-      ],
+              const SizedBox(height: 20),
+              Text(
+                "Longitud: $longitud",
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ],
+          );
+        }
+        return const CircularProgressIndicator();
+      },
     );
+  }
+  Widget _obteGinyDireccioVent(double direccioVent) {
+    // Aquesta funció ens retorna una giny que conté
+    // una icona i un text, amb la direcció i el nom del vent
+    // segons la seua direcció.
+    // Fem ús de `late` per indicar que assignarem el valor després
+    // a les variables.
+
+    late Icon icona;
+    late String nomVent;
+
+    if (direccioVent > 22.5 && direccioVent < 65.5) {
+      icona = const Icon(Icons.north_east);
+      nomVent = "Gregal";
+    } else if (direccioVent > 67.5 && direccioVent < 112.5) {
+      icona = const Icon(Icons.east);
+      nomVent = "Llevant";
+    } else if (direccioVent > 112.5 && direccioVent < 157.5) {
+      icona = const Icon(Icons.south_east);
+      nomVent = "Xaloc";
+    } else if (direccioVent > 157.5 && direccioVent < 202.5) {
+      icona = const Icon(Icons.south);
+      nomVent = "Migjorn";
+    } else if (direccioVent > 202.5 && direccioVent < 247.5) {
+      icona = const Icon(Icons.south_west);
+      nomVent = "Llebeig/Garbí";
+    } else if (direccioVent > 247.5 && direccioVent < 292.5) {
+      icona = const Icon(Icons.west);
+      nomVent = "Ponent";
+    } else if (direccioVent > 292.5 && direccioVent < 337.5) {
+      icona = const Icon(Icons.north_west);
+      nomVent = "Mestral";
+    } else {
+      icona = const Icon(Icons.north);
+      nomVent = "Tramuntana";
+    }
+    return Row(children: [
+      Text(
+        nomVent,
+        style: Theme.of(context).textTheme.headlineSmall,
+      ),
+      icona,
+    ]);
+  }
+
+  Widget _obtenirIconaOratge(String value) {
+    /*
+
+    Aquesta funció obté un giny amb una imatge que es
+    correspon al codi de l'oratge (Assolellat, núvo, etc.)
+
+    Existeix una gran quantitat de codis per a l'oratge, que podem
+    consultar a la web d'OpenMeteo:https://open-meteo.com/en/docs/dwd-api#weathervariables,
+    a l'apartat "WMO Weather interpretation codes (WW)"
+
+     */
+    Set<String> sol = <String>{"0"};
+    Set<String> pocsNuvols = <String>{"1", "2", "3"};
+    Set<String> nuvols = <String>{"45", "48"};
+    Set<String> plujasuau = <String>{"51", "53", "55"};
+    Set<String> pluja = <String>{
+      "61",
+      "63",
+      "65",
+      "66",
+      "67",
+      "80",
+      "81",
+      "82",
+      "95",
+      "96",
+      "99"
+    };
+    Set<String> neu = <String>{"71", "73", "75", "77", "85", "86"};
+
+    if (sol.contains(value)) {
+      return Image.asset("assets/icons/png/soleado.png");
+    }
+    if (pocsNuvols.contains(value)) {
+      return Image.asset("assets/icons/png/poco_nublado.png");
+    }
+    if (nuvols.contains(value)) {
+      return Image.asset("assets/icons/png/nublado.png");
+    }
+    if (plujasuau.contains(value)) {
+      return Image.asset("assets/icons/png/lluvia_debil.png");
+    }
+    if (pluja.contains(value)) {
+      return Image.asset("assets/icons/png/lluvia.png");
+    }
+    if (neu.contains(value)) {
+      return Image.asset("assets/icons/png/nieve.png");
+    }
+
+    return Image.asset("assets/icons/png/poco_nublado.png");
   }
 }
